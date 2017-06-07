59f4b199f9dfc2f0d806b9920d6bd4e62a818d41
Charles Burtwistle <charles.burtwistle@nist.gov>
2017-06-07 16:34:01 -0400

    changes filename to pathname git push

--- src/splcge_module.py
+++ src/splcge_module.py
@@ -217,9 +217,9 @@ class SimpleCGE:
         
         #Do we want the outputs the same as the demo/does it matter?
 
-    def model_output(self, filename):
+    def model_output(self, pathname):
         for v in self.instance.component_objects(Var, active=True):
-            with open(filename + str(v), 'w') as var_output:  
+            with open(pathname + str(v), 'w') as var_output:  
                 varobject = getattr(self.instance, str(v))
                 var_output.write ('{},{} \n'.format('Names', varobject ))
                 for index in varobject:
