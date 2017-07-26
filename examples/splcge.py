9596bd49303abb1427b18b2b6f6551e73e2a3b90
Charles Burtwistle <charles.burtwistle@nist.gov>
2017-07-26 12:14:58 -0400

    adds helpful comments

--- examples/splcge.py
+++ examples/splcge.py
@@ -9,27 +9,44 @@ sys.path.insert(0, '../src/')
 from splcge_model_def import SplModelDef
 from PyCGE import PyCGE
 
+# Choose which model to look at, and create a ModelDef object
 spl_model = SplModelDef()
 
+
+# Create a PyCGE object and load the ModelDef object into it
 testcge = PyCGE(spl_model)
 
+
+# Load the data by passing the path to the directory that contains it
 testcge.model_data('../data/splcge_data_dir')
 
+
+# Create a `base` instance and pass in a variable and index to fix the numeraire
 testcge.model_instance('pf', 'CAP')
 
+
+# Calibrate the `base` instance
 testcge.model_calibrate('minos','neos')
 
+
+# This allows the user to see all the params, their values, and what their docs
+print('#===========original============#')
 testcge.model_postprocess('params')
 
+# Modify a parameters
 testcge.model_modify_base('X0','MLK',0)
 
-testcge.model_modify_base('X0','BRD',0)
-
-testcge.model_modify_base('X0','MLK',None,undo=True)
-
+# View the params again, notice X0[MLK] is now set to 0
+print('#===========X0[MLK] set to 0============#')
 testcge.model_postprocess('params')
 
-testcge.model_postprocess('dill_instance', 'saved_instances')
+# By passing in `undo=True` a user can undo their last change
+testcge.model_modify_base('X0','MLK',None,undo=True)
+
+# View all params again. Notice that X0[MLK] was restored to its original value 
+print('#===========after the "undo"============#')
+testcge.model_postprocess('params')
+
 
 
 
