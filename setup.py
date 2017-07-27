a75d869eb992b51dceed01f39c3b378ed9deb42b
Charles Burtwistle <charles.burtwistle@nist.gov>
2017-07-27 14:32:26 -0400

    first attempt at packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 14:13:56 2017
+
+@author: cmb11
+"""
+
+from distutils.core import setup
+
+setup(
+        name='PyCGE',
+        version='0.1dev',
+        packages=['pycge']
+        )
